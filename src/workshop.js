8ad49471262898f5a79ff5bf545d6f70570bf697
SamCharpentier <samuelcharpentier@outlook.com>
2024-01-31 18:10:35 -0500

    v2

--- src/workshop.js
+++ src/workshop.js
@@ -1,8 +1,6 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
-import './styles/style.css'
-
 console.log('Welcome to Vite + JS + Webflow!')
+
+console.log('allo');
 
 createBadge()
 animateTitle()
